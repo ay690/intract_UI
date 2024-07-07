4a3832116aeefcbc18b2b1c9431da08af8f18a08
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-08 02:31:35 +0530

    update: responsive banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,20 @@
 import { FaArrowRight } from "react-icons/fa6";
+
 const Banner = () => {
   return (
     <section className="relative bg-[url('https://www.intract.io/assets/intract-academy-background-5547a705.png')] bg-cover bg-center bg-no-repeat z-0">
       <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
-      <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 justify-center">
+      <div className="relative justify-center max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-          <div className="flex justify-center items-center">
+          <div className="flex items-center justify-center">
             <img
               src="https://www.intract.io/assets/academy-animated-logo-57b2ae61.gif"
-              className="h-20 w-44 "
+              className="h-20 w-44"
+              alt="Intract Academy Logo"
             />
           </div>
-          <p className="mt-4 text-white max-w-lg sm:text-xl/relaxed">
+          <p className="max-w-lg mt-4 text-sm text-white sm:text-base md:text-lg lg:text-xl">
             Intract{" "}
             <span className="text-gray-600">
               users have together made more than
@@ -22,13 +24,15 @@ const Banner = () => {
             earn crypto!
           </p>
 
-          <div className="mt-8 flex flex-wrap gap-4 text-center items-center justify-center">
+          <div className="flex flex-wrap items-center justify-center gap-4 mt-8 text-center">
             <a
               href="#"
-              className="flex items-center justify-center w-full bg-[#6435e9] rounded px-12 py-3 text-sm font-medium text-white shadow focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+              className="flex items-center justify-center w-full bg-[#6435e9] rounded px-8 py-2 text-sm sm:px-12 sm:py-3 md:text-base font-medium text-white shadow focus:outline-none focus:ring sm:w-auto"
             >
-              <span>Get Started</span>
-              <FaArrowRight className="ml-2" color="white" />
+              <span className="flex items-center transition-transform transform hover:translate-x-1">
+                <span>Get Started</span>
+                <FaArrowRight className="ml-2" color="white" />
+              </span>
             </a>
           </div>
         </div>
