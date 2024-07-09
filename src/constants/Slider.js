a63ce4dc3cdb895cd1a4ba3962761ab32f29d19c
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-09 19:33:27 +0530

    update: slider functionality

---
+++ src/constants/Slider.js
@@ -0,0 +1,209 @@
+export const sliderData = [
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/ff5f87c6c2b54f0b8de31b0b8e876c52.jpg",
+    heading: "Introduction To Farcaster",
+    subHeading:
+      "Farcaster is a decentralized social network built on open protocols. It empowers users to own their identity, content, and social connections, giving them control over their online presence in a secure and transparent way.This Alpha-hub quest is an...",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/34071bd4c56a4136bcbb311d6360a6fb.jpg",
+    heading: "Introduction to Rabby Wallet",
+    subHeading:
+      "Rabby Wallet is a multi-chain web3 wallet developed by DeBank. It supports over 10,000 cryptocurrencies and NFTs on Ethereum and 141 EVM chains with a focus on security and user-friendli....",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/a4786d0caee64a0082a84b4099e4a13d.png",
+    heading: "Introduction to Zest Protocol",
+    subHeading:
+      "Zest Protocol is a decentralized lending platform on Bitcoin, offering transparent, on-chain credit solutions. It connects borrowers and lenders directly, enabling access to Bitcoin-native capital markets with secure, efficient, and permissionless finan....",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/18abe473d8ed481d86d934f38ecb3b77.png",
+    heading: "Introduction to Squid Router",
+    subHeading:
+      "SquidRouter is a Web3 dApp enabling seamless cross-chain swaps and liquidity management. It connects multiple blockchains, allowing users to trade assets efficiently and securely across diverse networks, enhancing inter...",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/4076c6ccc3734bbaac1b00cac6d9e524.png",
+    heading: "Exploring Sui Network",
+    subHeading:
+      "Explore the Sui Network, a decentralized blockchain platform designed for high-speed, secure transactions. It offers scalable infrastructure for dApps, smart contracts, and DeFi, enabling developers and users to create and engage in innovative digital solutions efficiently.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/1e0d8d85157848368a43ada6326421f6.jpg",
+    heading: "Introduction to Okto Wallet",
+    subHeading:
+      "Okto is a decentralized finance (DeFi) wallet by CoinDCX that provides a user-friendly platform to explore the Web3 ecosystem. With Okto, you can securely manage your crypto assets, swap tokens, access various DeFi protocols, and earn passive income. It's a one-stop shop for all your DeFi needs.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/c711648ae7a341e6a4d99f772b3cb95a.png",
+    heading: "Introduction to Mind Network",
+    subHeading:
+      "Mind Network is a cutting-edge Web3 platform leveraging Fully Homomorphic Encryption (FHE) to secure data and transactions. It offers enhanced privacy, enabling computation on encrypted data without decryption, and is developing a suite of interconnected products for a...",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/afbca71e1014419cbec2157ff6bc2ee9.jpg",
+    heading: "Introduction to DeBank",
+    subHeading:
+      "DeBank is a comprehensive dashboard designed for tracking and managing your assets within the world of DeFi. It offers insights into various DeFi protocols, stablecoins, exchanges, and lending platforms, empower...",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/83a56cb4554245b2a056ff711ef20eff.jpg",
+    heading: "Navigating Bungee and Jumper Exchanges",
+    subHeading:
+      "Bungee Exchange is a cross-chain swap platform optimizing token exchanges across blockchains. Jumper Exchange, powered by LI.FI, aggregates liquidity for multi-chain bridging and swapping, simplifying asset transfers.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/70e720677ec649cfa5ef27db26b3f73a.jpg",
+    heading: "Introduction to Immutable zkEVM",
+    subHeading:
+      "Immutable zkEVM is a blockchain for games, offering EVM compatibility, low cost, massive scalability, and Ethereum-level security, utilizing zk-rollup technology for enhanced performance in the gaming industry.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/b8b521250e044419be7ca01abc613db6.png",
+    heading: "Aperture Finance",
+    subHeading:
+      "Aperture Finance is a Silicon Valley startup focused on DeFi investments and protocol development, offering high-yield strategies and smart contract solutions to manage risks and scale operations efficiently.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/86d3cab87ef343a9a3dd1a309a7287c0.png",
+    heading: "Introduction to Elixir Protocol",
+    subHeading:
+      "Elixir protocols provide a way to implement polymorphic behavior across different data types or modules. They define a set of functions that adhering types can implement, enabling seamless interoperability and code reuse. 🚀🔗 ",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/c777114fc2884fa3a04f3c3c15bbadd5.png",
+    heading: "Navigating deBridge’s Point Realm 🌟🚀",
+    subHeading:
+      "Navigating deBridge’s Point System Realm 🚀 Earn points by making cross-chain transfers on deBridge and partner applications. For every $1 in fees, you’ll earn 100 points. Power up your governance influence and celebrate collective success! 🌟",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/633e3c0a8fd442b081e2a52320d7af5b.png",
+    heading: "Navigating Zora’s NFT Kingdom",
+    subHeading:
+      "Embark on a Zora adventure where digital treasures await. This guide is your map to the realm, leading you through mystical NFT landscapes and unlocking the secrets of blockchain bounty with wisdom and wit.",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/7112ab8471af4064a9ea7a4396bcfb22.png",
+    heading: "Getting Started with Monad",
+    subHeading:
+      "Monad is a high-performance, EVM compatible Layer 1 blockchain. It boosts efficiency with parallel execution, achieving 10,000 TPS 🚀. Monad maintains decentralization while scaling, offering developers and users a seamless experience with existing...",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+
+  {
+    imgSrc:
+      "https://static.highongrowth.xyz/enterprise/65017d014e5f24613adbfd67/33374b00ba544987938636e469c53115.png",
+    heading: "PARCL Real Estate",
+    subHeading:
+      "Where real estate meets crypto magic! Engage with PARCL protocols and unlock the door to a world of perpetuals trading and liquidity + qualify for future drops via your on-chain reputation 🏠🌟",
+    avatars: [
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80",
+      "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80",
+    ],
+  },
+];
