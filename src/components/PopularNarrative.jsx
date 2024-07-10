16bf81378730fbed0e40baada39d1862c398eff2
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-10 18:47:35 +0530

    update: see more button

--- src/components/PopularNarrative.jsx
+++ src/components/PopularNarrative.jsx
@@ -6,6 +6,11 @@ const PopularNarrative = () => {
       <Farcaster />
       <Lens />
       <Grass />
+      <div className="flex justify-center max-w-full mx-auto mt-3 ">
+        <button className="px-4 py-2 text-white bg-gray-500 rounded-lg hover:bg-gray-600 focus:outline-none hover:cursor-pointer">
+          See More
+        </button>
+      </div>
     </>
   );
 };
